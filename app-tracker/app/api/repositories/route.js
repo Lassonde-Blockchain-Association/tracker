13f6390e76b063682c4865e54db94bdb52e40fd0
Abdalla Sarhan <abdalla1@my.yorku.ca>
2024-10-16 16:05:48 -0400

    Retrieving enviroment variables in route

--- app-tracker/app/api/repositories/route.js
+++ app-tracker/app/api/repositories/route.js
@@ -1,4 +1,7 @@
 export async function GET(request) {
+	const githubApiToken = process.env.GITHUB_API_TOKEN;
+	const githubApiUrl = process.env.GITHUB_API_URL;
+
 	return new Response(
 		JSON.stringify({ message: "Hello from the API /api/repositories" }),
 		{
