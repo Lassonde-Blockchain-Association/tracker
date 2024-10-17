b9cd7a07db488e082bebfda658ece453b4643ea2
Joanne Wijetunga <joanne04@my.yorku.ca>
2024-10-16 22:29:14 -0400

    Created endpoint to get the list of repositories and the contributors given a repository

--- app-tracker/app/api/repositories/route.js
+++ app-tracker/app/api/repositories/route.js
@@ -2,12 +2,32 @@ export async function GET(request) {
 	const githubApiToken = process.env.GITHUB_API_TOKEN;
 	const githubApiUrl = process.env.GITHUB_API_URL;
 
-	return new Response(
-		JSON.stringify({ message: "Hello from the API /api/repositories" }),
-		{
-			headers: { "Content-Type": "application/json" },
-		}
-	);
+
+	try {
+		// Fetch repositories from GitHub API
+		const response = await axios({
+			method: 'get',
+			url: `${GITHUB_API_URL}/orgs/Lassonde-Blockchain-Association/repos`,
+			headers: {
+				'Accept': 'application/vnd.github+json',
+				'Authorization': `Bearer ${GITHUB_API_TOKEN}`,
+				'X-Github-Api-Version': '2022-11-28'
+			}
+		});
+
+		// List repositories
+		const repos = response.data;
+		
+		// Return the repositories in the response
+		return new Response(JSON.stringify(repos), {
+			headers: { 'Content-Type': 'application/json' }
+		});
+	} catch (error) {
+		// Handle any errors
+		console.error('Error fetching repositories:', error.message);
+		return new Response('Failed to fetch repositories', { status: 500 });
+	}
+
 }
 
 // Path: app-tracker/app/api/repositories/route.js
