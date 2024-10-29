a1ce54ee31af952f71dd6edfcbf4099d54b951b1
Abdalla Sarhan <abdalla1@my.yorku.ca>
2024-10-29 15:52:52 -0400

    Fetching repositories and displaying on the frontend

--- app-tracker/app/repositories/page.tsx
+++ app-tracker/app/repositories/page.tsx
@@ -1,10 +1,45 @@
-"use client"
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import "./repositories.css";
+import "../spinner.css";
 
 function Repositories() {
-  return (
-    <div>Repositories</div>
-  )
+	const [repositories, setRepositories] = useState<any[]>([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetch("/api/repositories")
+			.then((response) => response.json())
+			.then((data) => {
+				setRepositories(data);
+				setLoading(false);
+			})
+			.catch((error) => {
+				console.error("Error fetching data:", error);
+				setLoading(false);
+			});
+	}, []);
+
+	return (
+		<div className="repositories-container">
+			<h1>Repositories</h1>
+			{loading && <div className="spinner" />}
+			<div className="repositories-list">
+				{repositories.map((repository, index) => (
+					<div key={index} className="repository-card">
+						<h2>{repository.name}</h2>
+						<p>
+							Description:{" "}
+							{repository.description || "No description provided."}
+						</p>
+						<a href={repository.url} target="_blank" rel="noopener noreferrer">
+							View Repository
+						</a>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
-export default Repositories
+export default Repositories;
